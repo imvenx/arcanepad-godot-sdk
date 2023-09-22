5f558ce01a3a4cad0d86c5585ab4c25d35354f99
imvenx <imvenx@proton.me>
2023-09-22 17:04:16 +0200

    add aeventname and use it

---
+++ Arcane_Library/models/AEventName.gd
@@ -0,0 +1,51 @@
+class_name AEventName
+
+static var None = "None";
+
+static var Left = "Left";
+static var Right = "Right";
+static var Up = "Up";
+static var Down = "Down";
+static var Enter = "Enter";
+static var Info = "Info";
+static var Extra = "Extra";
+static var Back = "Back";
+
+static var SetDeviceType = "SetDeviceType";
+
+static var EnterDevMode = "EnterDevMode";
+
+static var EnterApp = "EnterApp";
+static var ExitApp = "ExitApp";
+
+static var RefreshGlobalState = "RefreshGlobalState";
+
+static var Initialize = "Initialize";
+
+static var ClientConnect = "ClientConnect";
+static var ClientDisconnect = "ClientDisconnect";
+
+static var IframePadConnect = "IframePadConnect";
+static var IframePadDisconnect = "IframePadDisconnect";
+
+static var StartGetQuaternion = "StartGetQuaternion";
+static var StopGetQuaternion = "StopGetQuaternion";
+static var GetQuaternion = "GetQuaternion";
+static var CalibrateQuaternion = "CalibrateQuaternion";
+
+static var StartGetRotationEuler = "StartGetRotationEuler";
+static var StopGetRotationEuler = "StopGetRotationEuler";
+static var GetRotationEuler = "GetRotationEuler";
+static var CalibrateRotationEuler = "CalibrateRotationEuler";
+
+static var StartGetPointer = "StartGetPointer";
+static var StopGetPointer = "StopGetPointer";
+static var GetPointer = "GetPointer";
+static var CalibratePointer = "CalibratePointer";
+
+static var Vibrate = "Vibrate";
+
+static var UpdateUser = "UpdateUser";
+
+static var OpenArcaneMenu = "OpenArcaneMenu";
+static var CloseArcaneMenu = "CloseArcaneMenu";
